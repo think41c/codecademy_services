8ef11691753b1883037a81e042d9105e49a4d1ec
Tony DiNitto <think41c@gmail.com>
2015-08-13 21:20:47 -0500

    Now saves the weather into .fiveDay.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,7 @@
 app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) { 
   // ... 
-}]);
+}]); 
+
+forecast.success(function(data) { 
+    $scope.fiveDay = data; 
+  });
